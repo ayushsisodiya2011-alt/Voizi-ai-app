4025570eb2476a9f2b1b43afbde23509d1132c02
ayushsisodiya2011-alt <ayushsisodiya2011@gmail.com>
2025-10-29 16:27:51 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+// Voizi Service Worker - Silent Startup Cache
+const CACHE_NAME = 'voizi-v1';
+const URLS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+// Install
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+// Fetch
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
